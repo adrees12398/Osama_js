49de99533b102e32e56b1cd9805f7fcb29962e66
Osama_Adrees <163242808+adrees12398@users.noreply.github.com>
2025-04-11 11:10:36 +0000

    comperison

---
+++ bascics01/comOperator.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2 >=1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//is traha conversion say avoid krna chiye
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined< 0);
+// console.log(undefined >= 0);
+// conversion easily ==
+console.log("2" == 2); // output true
+// === stricly checked datatype
+console.log("2" === 2); // outPut false
+
+
+
+
+
