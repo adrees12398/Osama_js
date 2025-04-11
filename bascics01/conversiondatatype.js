6766c2a3a93a7360b02b23603d8acc1a901f630b
Osama_Adrees <163242808+adrees12398@users.noreply.github.com>
2025-04-11 08:08:23 +0000

    operations & postfix & prefix

--- bascics01/conversiondatatype.js
+++ bascics01/conversiondatatype.js
@@ -6,11 +6,39 @@ let number = Number(score)
 // "33" => number
 // "33abc" =>NaN (Not a Number)
 
-let IsLoggIn = Osama
+let IsLoggIn = "Osama"
 let convert = Boolean(IsLoggIn)
-console.log(convert);
+// console.log(convert);
 //1 => true, 0 => false
 // "osama" =>  1 , " " => 0
+
+let someNumber = 33
+let stringconvert = String(someNumber)
+// console.log(typeof stringconvert);
+// *****************Operations****************
+let first_Name = "Osama"
+let last_Name = " Adrees"
+let fullName = first_Name + last_Name
+// console.log(fullName);
+let a = "1" + 2
+let b = 1 + "2" 
+let c = 1 + 2 + "2"
+let d = 1 + "2" + 2
+
+let e = num = num1 = num3 = 2 + 2  // output 4
+// console.log(num1);
+//prefix firstly addingorsubtracting the value than print the value but postfix firstly   addingorsubstracting the value than print the value
+let f = 10
+let g = --f
+console.log(f);
+console.log(g);
+
+
+
+
+
+
+
 
 
 
