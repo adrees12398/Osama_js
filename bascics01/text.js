103334fa6a35e2e3925ad93802e2643f5fc92905
Osama_Adrees <163242808+adrees12398@users.noreply.github.com>
2025-04-11 04:16:48 +0000

    commet

--- bascics01/text.js
+++ bascics01/text.js
@@ -1,1 +1,7 @@
-console.log("Hassan"); 
+const a = 12345;
+let accountName = "Osama";
+var city = "Lahore"
+Accountdetail = "Debet account"
+let AccountState;
+console.table([a,accountName,city,Accountdetail,AccountState])
+
