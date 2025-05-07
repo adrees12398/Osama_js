73226cb69ec586889157cbd2de982432369055fe
Osama_Adrees <163242808+adrees12398@users.noreply.github.com>
2025-05-07 21:03:01 +0000

    datetimeset

---
+++ bascics01/dateset.js
@@ -0,0 +1,24 @@
+const date = new Date();
+// date.setHours(date.getHours() + 5);
+// console.log(date.toString());
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleString())
+// const MyDate = new Date(2023,1,23)
+// const MyCreatedDate = new Date("01-14-2023")
+// const MyTimeStemp =  Date.now()
+// console.log(MyTimeStemp);
+// console.log(Math.floor(Date.now()/1000))
+const MyTimeStemp = new Date()
+console.log(MyTimeStemp.getMonth()+1);
+ MyTimeStemp.setHours(MyTimeStemp.getHours() + 5)
+console.log(MyTimeStemp.toString());
+
+
+
+
+
+
+
+// const year = date.getFullYear()
+// console.log(year)
+ 
